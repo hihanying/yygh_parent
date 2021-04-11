ab7a7047c161a670f2a7e0783327f5a3ddab144d
hanying <hihanying@foxmail.com>
2021-04-11 22:48:35 +0800

    Initial commit ：项目框架搭建

---
+++ src/main/java/top/hihanying/yygh/YyghApplication.java
@@ -0,0 +1,13 @@
+package top.hihanying.yygh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyghApplication.class, args);
+    }
+
+}
