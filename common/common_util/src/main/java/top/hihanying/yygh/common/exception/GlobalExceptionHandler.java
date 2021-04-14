6f7c07007a962f1b8f002177446c89006101c487
hanying <hihanying@foxmail.com>
2021-04-14 17:22:18 +0800

    医院设置微服务常用 API 完成

--- common/common_util/src/main/java/top/hihanying/yygh/common/exception/GlobalExceptionHandler.java
+++ common/common_util/src/main/java/top/hihanying/yygh/common/exception/GlobalExceptionHandler.java
@@ -7,14 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    /**
+     * 全局异常处理
+     * @param e 异常
+     * @return Result.fail()
+     */
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result error(Exception e) {
         e.printStackTrace();
         return Result.fail();
     }
-
+    /**
+     * 自定义异常处理
+     * @param e 自定义异常
+     * @return Result.fail()
+     */
     @ExceptionHandler(YyghException.class)
     @ResponseBody
     public Result error(YyghException e) {
--- service/service_hosp/src/main/java/top/hihanying/yygh/hosp/config/HospConfig.java
+++ service/service_hosp/src/main/java/top/hihanying/yygh/hosp/config/HospConfig.java
@@ -1,12 +1,10 @@
 package top.hihanying.yygh.hosp.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("top.hihanying.yygh.hosp.mapper")
-@ComponentScan("top.hihanying") // 扫描swagger
+@ComponentScan(basePackages = "top.hihanying.yygh") // 扫描swagger
 public class HospConfig {
 
 }
